612fa102cf6f4532adee26b8c2627c586d6fbc26
vishesh211 <visheshtiwari211@gmail.com>
2023-04-17 23:23:09 +0530

    feat: created MainActivity.kt

--- app/src/main/java/com/example/newsapplicationcompose/presentation/MainActivity.kt
+++ app/src/main/java/com/example/newsapplicationcompose/presentation/MainActivity.kt
@@ -1,15 +1,13 @@
-package com.example.newsapplicationcompose.activity
+package com.example.newsapplicationcompose.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.newsapplicationcompose.navigation.MainScreenNavigationConfigurations
 import com.example.newsapplicationcompose.ui.theme.NewsApplicationComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,15 +16,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             NewsApplicationComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
     }
+}
+
+@Composable
+private fun MainScreen() {
+    val navController = rememberNavController()
+
+    MainScreenNavigationConfigurations()
 }
 
 @Composable
