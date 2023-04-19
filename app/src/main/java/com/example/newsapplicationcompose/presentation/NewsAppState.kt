5f0af5d65f929c2d7a0488312f5381c821f021e0
vishesh211 <visheshtiwari211@gmail.com>
2023-04-19 15:57:14 +0530

    feat: created class

--- app/src/main/java/com/example/newsapplicationcompose/presentation/NewsAppState.kt
+++ app/src/main/java/com/example/newsapplicationcompose/presentation/NewsAppState.kt
@@ -3,7 +3,12 @@ package com.example.newsapplicationcompose.presentation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.rememberNavController
 
 @Composable
@@ -20,4 +25,25 @@ class NewsAppState(val navController: NavController) {
 
     val currentRoute: String?
         get() = navController.currentDestination?.route
+
+    fun navigationToBottomBarRoute(route: String) {
+        if (route != currentRoute) {
+            navController.navigate(route) {
+                launchSingleTop = true
+                restoreState = true
+
+                popUpTo(findStartDestination(navController.graph).id)
+            }
+        }
+    }
+
+    private fun NavBackStackEntry.lifecycleResumed() =
+        this.getLifecycle().currentState == Lifecycle.State.RESUMED
+
+    private val NavGraph.startDestination: NavDestination?
+        get() = findNode(startDestinationId)
+
+    private tailrec fun findStartDestination(graph: NavDestination): NavDestination {
+        return if (graph is NavGraph) findStartDestination(graph.findStartDestination()!!) else graph
+    }
 }
