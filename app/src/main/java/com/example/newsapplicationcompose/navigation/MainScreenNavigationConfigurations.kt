380076c640d706df227a43cf58e9061f8cab57dd
vishesh211 <visheshtiwari211@gmail.com>
2023-04-19 22:12:22 +0530

    feat: created function NewsSurface

--- app/src/main/java/com/example/newsapplicationcompose/navigation/MainScreenNavigationConfigurations.kt
+++ app/src/main/java/com/example/newsapplicationcompose/navigation/MainScreenNavigationConfigurations.kt
@@ -1,11 +1,18 @@
 package com.example.newsapplicationcompose.navigation
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.newsapplicationcompose.presentation.components.NewsScaffold
+import com.example.newsapplicationcompose.ui.theme.NewsApplicationComposeTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -19,10 +26,40 @@ fun MainScreenNavigationConfigurations() {
             )
         },
         bottomBar = {
-
+            NewsBottomBar(
+            )
         }
 
     )
+}
+
+@Composable
+fun NewsBottomBar(
+    tabs: Array<BottomNavigationScreens>,
+    currentRoute: String,
+    navigateToRoute: (String) -> Unit,
+    color: Color = MaterialTheme.colorScheme.primaryContainer,
+    contentColor: Color = MaterialTheme.colorScheme.primary
+) {
+    val routes = tabs.map { it.route }
+    val currentSection = tabs.first { it.route == currentRoute }
+
+    NewsSurface(
+
+    )
+}
+
+@Composable
+fun NewsSurface(
+    modifier: Modifier = Modifier,
+    shape: Shape = RectangleShape,
+    color: Color = MaterialTheme.colorScheme.primaryContainer,
+    contentColor: Color = MaterialTheme.colorScheme.primary,
+    border: BorderStroke? = null,
+    elevation: Dp = 0.dp,
+    content: @Composable () -> Unit
+) {
+    TODO("Not yet implemented")
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
