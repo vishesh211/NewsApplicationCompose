adb3d30fc88a3c184291a7ae16423565fb8feee8
vishesh211 <visheshtiwari211@gmail.com>
2023-04-19 15:22:38 +0530

    feat: created class

--- app/src/main/java/com/example/newsapplicationcompose/presentation/NewsAppState.kt
+++ app/src/main/java/com/example/newsapplicationcompose/presentation/NewsAppState.kt
@@ -1,2 +1,23 @@
 package com.example.newsapplicationcompose.presentation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun rememberNewsAppState(
+    navController: NavController = rememberNavController()
+) =
+    remember(navController) {
+        NewsAppState(navController)
+    }
+
+
+@Stable
+class NewsAppState(val navController: NavController) {
+
+    val currentRoute: String?
+        get() = navController.currentDestination?.route
+}
