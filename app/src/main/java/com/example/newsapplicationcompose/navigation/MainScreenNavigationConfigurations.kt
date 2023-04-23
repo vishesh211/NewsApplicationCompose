6976665963ad8f0b4ead0a7fb63518580e771856
vishesh211 <visheshtiwari211@gmail.com>
2023-04-23 19:53:11 +0530

    feat: added lerp function

--- app/src/main/java/com/example/newsapplicationcompose/navigation/MainScreenNavigationConfigurations.kt
+++ app/src/main/java/com/example/newsapplicationcompose/navigation/MainScreenNavigationConfigurations.kt
@@ -1,25 +1,31 @@
 package com.example.newsapplicationcompose.navigation
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.SpringSpec
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.lerp
 import androidx.compose.ui.zIndex
 import com.example.newsapplicationcompose.presentation.components.NewsScaffold
-import com.example.newsapplicationcompose.ui.theme.NewsApplicationComposeTheme
+import kotlin.math.roundToInt
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -55,7 +61,81 @@ fun NewsBottomBar(
         color = color,
         contentColor = contentColor
     ) {
+        val springSpec = SpringSpec<Float>(
+            stiffness = 800f,
+            dampingRatio = 0.8f
+        )
+        NewsBottomNavLayout()
+    }
+}
 
+@Composable
+fun NewsBottomNavLayout(
+    selectedIndex: Int,
+    itemCount: Int,
+    animSpec: AnimationSpec<Float>,
+    indicator: @Composable BoxScope.() -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    val selectionFractions = remember(itemCount) {
+        List(itemCount) { i ->
+            Animatable(if (i == selectedIndex) 1f else 0f)
+        }
+    }
+    selectionFractions.forEachIndexed { index, selectionFraction ->
+        val target = if (index == selectedIndex) 1f else 0f
+        LaunchedEffect(target, animSpec) {
+            selectionFraction.animateTo(target, animSpec)
+        }
+    }
+
+    val indicatorIndex = remember {
+        Animatable(0f)
+    }
+    val targetIndicatorIndex = selectedIndex.toFloat()
+    LaunchedEffect(targetIndicatorIndex) {
+        indicatorIndex.animateTo(targetIndicatorIndex, animSpec)
+    }
+
+    Layout(
+        modifier = Modifier.height(56.dp),
+        content = {
+            content()
+            Box(Modifier.layoutId("indicator"), content = indicator)
+        }
+    ) { measurables, constraints ->
+        check(itemCount == (measurables.size - 1))
+
+        val unselectedWidth = constraints.maxWidth / (itemCount + 1)
+        val selectedWidth = 2 * unselectedWidth
+        val indicatorMeasurable = measurables.first { it.layoutId == "indicator" }
+
+        val itemPlaceables = measurables
+            .filterNot { it == indicatorMeasurable }
+            .mapIndexed { index, measurable ->
+                val width = lerp(unselectedWidth, selectedWidth, selectionFractions[index].value)
+                measurable.measure(constraints.copy(minWidth = width, maxWidth = width))
+            }
+        val indicatorPlaceable = indicatorMeasurable.measure(
+            constraints.copy(
+                minWidth = selectedWidth,
+                maxWidth = selectedWidth
+            )
+        )
+
+        layout(
+            width = constraints.maxWidth,
+            height = itemPlaceables.maxByOrNull { it.height }?.height?: 0
+        ){
+            val indicatorLeft = indicatorIndex.value * unselectedWidth
+            indicatorPlaceable.placeRelative(x = indicatorLeft.toInt(), y = 0)
+            var x = 0
+            itemPlaceables.forEach { placeable ->
+                placeable.placeRelative(x = x, y = 0)
+                x += placeable.width
+            }
+        }
     }
 }
 
@@ -101,4 +181,8 @@ fun NewsTopBar(
         colors = colors,
         scrollBehavior = scrollBehavior
     )
+}
+
+fun lerp(start: Int, stop: Int, fraction: Float): Int {
+    return start + ((stop - start) * fraction.toDouble()).roundToInt()
 }
