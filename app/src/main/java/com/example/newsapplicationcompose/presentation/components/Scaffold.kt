e0ed0b5f1b09cc5ea141082ae48ba0219ce351ea
vishesh211 <visheshtiwari211@gmail.com>
2023-04-30 02:32:04 +0530

    feat: added snackbarhost

--- app/src/main/java/com/example/newsapplicationcompose/presentation/components/Scaffold.kt
+++ app/src/main/java/com/example/newsapplicationcompose/presentation/components/Scaffold.kt
@@ -13,7 +13,7 @@ fun NewsScaffold(
     modifier: Modifier = Modifier,
     topBar: @Composable () -> Unit = {},
     bottomBar: @Composable () -> Unit = {},
-    snackbarHost: @Composable () -> Unit = {},
+    snackbarHost: @Composable (SnackbarHostState) -> Unit = { SnackbarHost(it) },
     floatingActionButton: @Composable () -> Unit = {},
     floatingActionButtonPosition: FabPosition = FabPosition.End,
     containerColor: Color = MaterialTheme.colorScheme.background,
