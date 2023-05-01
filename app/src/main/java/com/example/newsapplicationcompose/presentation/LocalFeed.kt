84212fbeac952bdb21082dc8772a104a7539f7e3
vishesh211 <visheshtiwari211@gmail.com>
2023-05-01 14:19:46 +0530

    feat: created Feed

---
+++ app/src/main/java/com/example/newsapplicationcompose/presentation/LocalFeed.kt
@@ -0,0 +1,12 @@
+package com.example.newsapplicationcompose.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Feed(
+    onSnackClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+}
