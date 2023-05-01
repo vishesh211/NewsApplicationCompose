605b1ace61d80e0349b053e323a00e902e5207b8
vishesh211 <visheshtiwari211@gmail.com>
2023-05-01 16:13:25 +0530

    fix: deleted unnecessary stuff

--- app/src/main/java/com/example/newsapplicationcompose/navigation/MainScreenNavigationConfigurations.kt
+++ app/src/main/java/com/example/newsapplicationcompose/navigation/MainScreenNavigationConfigurations.kt
@@ -67,7 +67,9 @@ fun MainScreenNavigationConfigurations() {
                 startDestination = BottomNavigationScreens.BreakingNewsScreen.route,
                 modifier = Modifier.padding(innerPaddingModifier)
             ) {
-                newsNavGraph()
+                newsNavGraph(
+                    onNewsSelected = appState::na
+                )
             }
         }
     }
@@ -93,135 +95,7 @@ fun NewsBottomBar(
     color: Color = MaterialTheme.colorScheme.primaryContainer,
     contentColor: Color = MaterialTheme.colorScheme.primary
 ) {
-    val routes = tabs.map { it.route }
-    val currentSection = tabs.first { it.route == currentRoute }
 
-    NewsSurface(
-        color = color,
-        contentColor = contentColor
-    ) {
-        val springSpec = SpringSpec<Float>(
-            stiffness = 800f,
-            dampingRatio = 0.8f
-        )
-        NewsBottomNavLayout(
-            selectedIndex = currentSection.order,
-            itemCount = routes.size,
-            animSpec = springSpec,
-            indicator = {
-
-            },
-            modifier = Modifier.navigationBarsPadding()
-        ) {
-
-        }
-    }
-}
-
-@Composable
-private fun NewsBottomNavIndicator(
-    strokeWidth: Dp = 2.dp,
-    color: Color = NewsApplicationComposeTheme.colors.iconInteractive,
-    shape: Shape = BottomNavIndicatorShape
-) {
-    Spacer(
-        modifier = Modifier
-            .fillMaxSize()
-            .then(BottomNavigationItemPadding)
-            .border(strokeWidth, color, shape)
-    )
-}
-
-@Composable
-fun NewsBottomNavLayout(
-    selectedIndex: Int,
-    itemCount: Int,
-    animSpec: AnimationSpec<Float>,
-    indicator: @Composable BoxScope.() -> Unit,
-    modifier: Modifier = Modifier,
-    content: @Composable () -> Unit
-) {
-    val selectionFractions = remember(itemCount) {
-        List(itemCount) { i ->
-            Animatable(if (i == selectedIndex) 1f else 0f)
-        }
-    }
-    selectionFractions.forEachIndexed { index, selectionFraction ->
-        val target = if (index == selectedIndex) 1f else 0f
-        LaunchedEffect(target, animSpec) {
-            selectionFraction.animateTo(target, animSpec)
-        }
-    }
-
-    val indicatorIndex = remember {
-        Animatable(0f)
-    }
-    val targetIndicatorIndex = selectedIndex.toFloat()
-    LaunchedEffect(targetIndicatorIndex) {
-        indicatorIndex.animateTo(targetIndicatorIndex, animSpec)
-    }
-
-    Layout(
-        modifier = Modifier.height(56.dp),
-        content = {
-            content()
-            Box(Modifier.layoutId("indicator"), content = indicator)
-        }
-    ) { measurables, constraints ->
-        check(itemCount == (measurables.size - 1))
-
-        val unselectedWidth = constraints.maxWidth / (itemCount + 1)
-        val selectedWidth = 2 * unselectedWidth
-        val indicatorMeasurable = measurables.first { it.layoutId == "indicator" }
-
-        val itemPlaceables = measurables
-            .filterNot { it == indicatorMeasurable }
-            .mapIndexed { index, measurable ->
-                val width = lerp(unselectedWidth, selectedWidth, selectionFractions[index].value)
-                measurable.measure(constraints.copy(minWidth = width, maxWidth = width))
-            }
-        val indicatorPlaceable = indicatorMeasurable.measure(
-            constraints.copy(
-                minWidth = selectedWidth,
-                maxWidth = selectedWidth
-            )
-        )
-
-        layout(
-            width = constraints.maxWidth,
-            height = itemPlaceables.maxByOrNull { it.height }?.height ?: 0
-        ) {
-            val indicatorLeft = indicatorIndex.value * unselectedWidth
-            indicatorPlaceable.placeRelative(x = indicatorLeft.toInt(), y = 0)
-            var x = 0
-            itemPlaceables.forEach { placeable ->
-                placeable.placeRelative(x = x, y = 0)
-                x += placeable.width
-            }
-        }
-    }
-}
-
-@Composable
-fun NewsSurface(
-    modifier: Modifier = Modifier,
-    shape: Shape = RectangleShape,
-    color: Color = MaterialTheme.colorScheme.primaryContainer,
-    contentColor: Color = MaterialTheme.colorScheme.primary,
-    border: BorderStroke? = null,
-    elevation: Dp = 0.dp,
-    content: @Composable () -> Unit
-) {
-    Box(
-        modifier = Modifier
-            .shadow(elevation = elevation, shape = shape, clip = false)
-            .zIndex(elevation.value)
-            .then(if (border != null) Modifier.border(border, shape) else Modifier)
-            .background(color = color, shape = shape)
-            .clip(shape)
-    ) {
-        CompositionLocalProvider(LocalContentColor provides contentColor, content = content)
-    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -243,31 +117,6 @@ fun NewsTopBar(
         windowInsets = windowInsets,
         colors = colors,
         scrollBehavior = scrollBehavior
-    )
-}
-
-@Composable
-fun NewsSnackBar(
-    snackbarData: SnackbarData,
-    modifier: Modifier = Modifier,
-    actionOnNewLine: Boolean = false,
-    shape: Shape = MaterialTheme.shapes.small,
-    containerColor: Color = MaterialTheme.colorScheme.primaryContainer,
-    contentColor: Color = MaterialTheme.colorScheme.onBackground,
-    actionColor: Color = MaterialTheme.colorScheme.onBackground,
-    actionContentColor: Color = MaterialTheme.colorScheme.onBackground,
-    dismissActionContentColor: Color = MaterialTheme.colorScheme.primary,
-) {
-    Snackbar(
-        snackbarData = snackbarData,
-        modifier = modifier,
-        actionOnNewLine = actionOnNewLine,
-        shape = shape,
-        containerColor = containerColor,
-        contentColor = contentColor,
-        actionColor = actionColor,
-        actionContentColor = actionContentColor,
-        dismissActionContentColor = dismissActionContentColor
     )
 }
 
