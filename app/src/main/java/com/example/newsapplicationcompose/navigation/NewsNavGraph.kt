de401331c3852a89af36bfe160734dd9f75cb13e
vishesh211 <visheshtiwari211@gmail.com>
2023-05-02 23:10:38 +0530

    feat: completed NewsNavGraph.kt

--- app/src/main/java/com/example/newsapplicationcompose/navigation/NewsNavGraph.kt
+++ app/src/main/java/com/example/newsapplicationcompose/navigation/NewsNavGraph.kt
@@ -1,2 +1,34 @@
 package com.example.newsapplicationcompose.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun NewsNavGraph(
+    navHostController: NavHostController,
+    startDestination: String,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navHostController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        composable(route = BottomNavigationScreens.BreakingNewsScreen.route) {
+            BreakingNewsScreen()
+        }
+        composable(route = BottomNavigationScreens.GlobalNewsScreen.route) {
+            GlobalNewsScreen()
+        }
+        composable(route = BottomNavigationScreens.SearchScreen.route) {
+            SearchScreenRoute()
+        }
+        composable(route = BottomNavigationScreens.BookMarksScreen.route) {
+            BookMarksScreen()
+        }
+    }
+}
