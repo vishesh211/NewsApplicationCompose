1c24b7b616fb97a6a0ed1dde6e3ecf1defe0a436
vishesh211 <visheshtiwari211@gmail.com>
2023-05-04 00:01:14 +0530

    feat: added Card in NewsItem

--- app/src/main/java/com/example/newsapplicationcompose/presentation/breakingnews/components/Utils.kt
+++ app/src/main/java/com/example/newsapplicationcompose/presentation/breakingnews/components/Utils.kt
@@ -1,8 +1,29 @@
 package com.example.newsapplicationcompose.presentation.breakingnews.components
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewsItem() {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        shape = MaterialTheme.shapes.extraSmall,
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        onClick = {
 
+        }
+    ) {
+
+    }
 }
