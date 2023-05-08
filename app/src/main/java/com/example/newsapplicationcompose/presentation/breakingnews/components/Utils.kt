3e271bafc2b044ab450e94deb8324f182b6c3f41
vishesh211 <visheshtiwari211@gmail.com>
2023-05-08 23:02:39 +0530

    feat: created NewsItemCard

--- app/src/main/java/com/example/newsapplicationcompose/presentation/breakingnews/components/Utils.kt
+++ app/src/main/java/com/example/newsapplicationcompose/presentation/breakingnews/components/Utils.kt
@@ -1,28 +1,37 @@
 package com.example.newsapplicationcompose.presentation.breakingnews.components
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NewsItem() {
+fun NewsItemCard(
+    onClick: () -> Unit,
+    isBookMarked: Boolean,
+    onToggleBookMark: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Card(
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        onClick = onClick,
         modifier = Modifier
             .padding(8.dp)
-            .fillMaxWidth(),
-        shape = MaterialTheme.shapes.extraSmall,
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
-        onClick = {
-
-        }
     ) {
-
+        Row(horizontalArrangement = Arrangement.SpaceAround) {
+            Column {
+                NewsSource()
+                Spacer(modifier = Modifier.height(12.dp))
+                NewsHeadline()
+                Spacer(modifier = Modifier.height(12.dp))
+                NewsPublished()
+            }
+        }
     }
 }
