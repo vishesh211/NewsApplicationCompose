41c22de1795f4cd8fa9c206993de72a5b0435b77
vishesh211 <visheshtiwari211@gmail.com>
2023-05-08 23:25:07 +0530

    feat: got topArticles from viewmodel

--- app/src/main/java/com/example/newsapplicationcompose/presentation/breakingnews/BreakingNewsScreen.kt
+++ app/src/main/java/com/example/newsapplicationcompose/presentation/breakingnews/BreakingNewsScreen.kt
@@ -1,15 +1,20 @@
 package com.example.newsapplicationcompose.presentation.breakingnews
 
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import com.example.newsapplicationcompose.presentation.breakingnews.components.NewsItemCard
 import com.example.newsapplicationcompose.viewmodel.NewsViewModel
 
 @Composable
 fun BreakingNewsScreen(viewModel: NewsViewModel) {
+    viewModel.getTopArticles()
+    val topArticles = viewModel.topArticles.value
     LazyColumn() {
-        items(items = ) {
-            NewsItemCard()
+        items(items = topArticles) {
+            NewsItemCard(
+                newSource =
+            )
         }
     }
 }
